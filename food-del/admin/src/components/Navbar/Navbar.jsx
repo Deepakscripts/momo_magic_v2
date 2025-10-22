32c6a6867b852d18d00ff1f5614fe61cc703aedc
Deepak agrawal <aadeepak05@gmail.com>
2025-10-22 15:33:04 +0530

    Login added in admin panel

--- food-del/admin/src/components/Navbar/Navbar.jsx
+++ food-del/admin/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,44 @@
-import React from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
+import React from "react";
+import "./Navbar.css";
+import { useAuth } from "../../auth/AuthContext";
 
-const Navbar = ({ onHamburgerClick }) => {
+const Navbar = ({ onHamburgerClick, onAvatarClick }) => {
+  const { user, logout } = useAuth();
+
   return (
-    <div className='navbar'>
-      {/* Hamburger only shows on small screens */}
-      <button
-        className="hamburger"
-        aria-label="Open menu"
-        onClick={onHamburgerClick}
-      >
-        <span />
-        <span />
-        <span />
-      </button>
+    <div className="navbar">
+      <div className="nav-left">
+        <button className="hamburger" onClick={onHamburgerClick} aria-label="Menu">
+          ☰
+        </button>
+        <div className="brand">
+          <span className="brand-name">Tomato.</span>
+          <span className="brand-sub">Admin Panel</span>
+        </div>
+      </div>
 
-      <img className='logo' src={assets.logo} alt="logo" />
-      <img className='profile' src={assets.profile_image} alt="profile" />
+      <div className="nav-right">
+        {/* avatar + caption */}
+        <div className="avatar-wrap" onClick={onAvatarClick} role="button" tabIndex={0}
+             onKeyDown={(e)=>{ if(e.key==='Enter'){ onAvatarClick?.(); }}}>
+          <img
+            className="avatar"
+            src="https://i.pravatar.cc/48?img=12"
+            alt="Account"
+          />
+          <div className="avatar-caption">
+            {user ? user.display : "Login"}
+          </div>
+        </div>
+
+        {user ? (
+          <button className="logout-btn" onClick={logout} title="Sign out">
+            Logout
+          </button>
+        ) : null}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
